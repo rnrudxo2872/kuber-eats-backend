44274eb877dc64aa09215fc84113ffbe1e563181
unknown <rnrudxo2872@naver.com>
2021-07-14 22:15:26 +0900

    resolver Args decorator 사용

--- src/restaurant/restaurants.resolver.ts
+++ src/restaurant/restaurants.resolver.ts
@@ -1,10 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query((_) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((_) => [Restaurant])
+  myRestaurant(
+    @Args('veganOnly', { nullable: true }) veganOnly: boolean,
+  ): Restaurant[] {
+    console.log(veganOnly);
+
+    return [];
   }
 }

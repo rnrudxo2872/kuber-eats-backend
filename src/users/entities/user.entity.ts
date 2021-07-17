2a6316725c24b4675bcf7a36adecc3f9b0a870ab
unknown <rnrudxo2872@naver.com>
2021-07-17 22:46:33 +0900

    createAccount service method 작성

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,7 +3,11 @@ import { IsString } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
-type UserRole = 'client' | 'owner' | 'del';
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
 
 @ObjectType()
 @Entity()
@@ -18,7 +22,7 @@ export class User extends CoreEntity {
   @Field((_) => String)
   password: string;
 
-  @Column()
-  @Field((_) => String)
+  @Column({ type: 'enum', enum: UserRole })
+  @Field((_) => UserRole)
   role: UserRole;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { createAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -10,5 +11,21 @@ export class UsersService {
   ) {}
   getAll() {
     return true;
+  }
+
+  async createAccount({ email, password, role }: createAccountInput) {
+    try {
+      const exists = this.Users.findOne({ email });
+
+      if (exists) {
+        //exists Email => make error
+        return;
+      }
+      this.Users.save(this.Users.create({ email, password, role }));
+      return true;
+    } catch (e) {
+      console.log(e);
+      return;
+    }
   }
 }

30ce92cfcb8b6ba286fbc5db20fa9aa41e307339
unknown <rnrudxo2872@naver.com>
2021-07-23 18:31:10 +0900

    mocking을 이용하여 가짜 repository와 service 제공, jest가 읽지 못하는 경로 package.json에 경로 설정(moduleNameMapper)

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,10 +1,67 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { VerificationService } from 'src/verification/verification.service';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockVerificationService = {
+  create: jest.fn(),
+  findOne: jest.fn(),
+  verifyEmail: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerifyEmail: jest.fn(),
+};
+
 describe('UserService', () => {
-  beforeAll(() => {
-    const module = Test.createTestingModule({
-      imports: [UsersService],
-    });
+  let service: UsersService;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: VerificationService,
+          useValue: mockVerificationService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
+    }).compile();
+    service = await module.get<UsersService>(UsersService);
   });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it.todo('getAll');
+  it.todo('createAccount');
+  it.todo('login');
+  it.todo('getById');
+  it.todo('editUser');
 });
